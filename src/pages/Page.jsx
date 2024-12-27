d62b5edab87287d0d09fbf79bbf115b95abc5c83
Askold <makrikovas@gmail.com>
2024-12-27 02:42:13 +0200

    sleeptime

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,11 +1,14 @@
 import '@/styles/pages/page.scss'
 import { UserControls } from '@/ui/sections/UserControls'
+import { Preloader } from '@/ui/components/Preloader'
 
-export function Page({ children }) {
+export function Page({ children, loading }) {
   return (
-  <>
-  <UserControls />
-  <div className="page">{children}</div>
-  </>
+    <>
+      <UserControls />
+      <div className="page">
+        {loading ? <Preloader /> : children}
+      </div>
+    </>
   )
 }
